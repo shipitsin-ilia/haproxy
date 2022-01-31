shipitsin-ilia/haproxy

commit b5b5247b18cd2d1f81d577b1418cf8df202b2ca1

MINOR: quic: Immediately close if no transport parameters extension found
If the ClientHello callback does not manage to find a correct QUIC transport
parameters extension, we immediately close the connection with
missing_extension(109) as TLS alert which is turned into 0x16d QUIC connection
error.

src/ssl_sock.c
@@ -2463,7 +2463,15 @@ int ssl_sock_switchctx_cbk(SSL *ssl, int *al, void *arg)
 		if (!SSL_client_hello_get0_ext(ssl, conn->qc->tps_tls_ext,
 		                               &extension_data, &extension_len))
 #endif
-			goto abort;
+		{
+			/* This is not redundant. It we only return 0 without setting
+			 * <*al>, this has as side effect to generate another TLS alert
+			 * which would be set after calling quic_set_tls_alert().
+			 */
+			*al = SSL_AD_MISSING_EXTENSION;
+			quic_set_tls_alert(conn->qc, SSL_AD_MISSING_EXTENSION);
+			return 0;
+		}
 
 		if (!quic_transport_params_store(conn->qc, 0, extension_data,
 		                                 extension_data + extension_len))

commit 4e160be34699dee2da68d2a9b403aa0c4333e33e

BUILD: SSL: fix BoringSSL guarding
commit b5b5247b18cd2d1f81d577b1418cf8df202b2ca1 has introduced change
that breaks BoringSSL builds

src/ssl_sock.c
@@ -2506,10 +2506,10 @@ int ssl_sock_switchctx_cbk(SSL *ssl, int *al, void *arg)
 #ifdef OPENSSL_IS_BORINGSSL
 		if (!SSL_early_callback_ctx_extension_get(ctx, qc->tps_tls_ext,
 		                                          &extension_data, &extension_len))
+		goto abort;
 #else
 		if (!SSL_client_hello_get0_ext(ssl, qc->tps_tls_ext,
 		                               &extension_data, &extension_len))
-#endif
 		{
 			/* This is not redundant. It we only return 0 without setting
 			 * <*al>, this has as side effect to generate another TLS alert
@@ -2519,6 +2519,7 @@ int ssl_sock_switchctx_cbk(SSL *ssl, int *al, void *arg)
 			quic_set_tls_alert(qc, SSL_AD_MISSING_EXTENSION);
 			return 0;
 		}
+#endif
 
 		if (!quic_transport_params_store(qc, 0, extension_data,
 		                                 extension_data + extension_len))